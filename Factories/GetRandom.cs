b51e1debdf27539136e8bc6fe2918173671d0418
Kicka5h <karczag.ashley@gmail.com>
2021-02-25 12:37:36 -0800

    started building the random pattern ctor

---
+++ Factories/GetRandom.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuiltySpark.Factories
+{
+    public class GetRandom
+    {
+        public class UNSC {
+
+            // Name will contain the constructors for each pattern of name
+            //each ctor will randomly pull from the text list 
+            //Name will randomly choose which ctor to use
+            public string Name()
+            {
+                string path = "..\\Factories\\UNSC\\Constructors";
+                string[] allLines = File.ReadAllLines(path);
+
+                string name = allLines[new Random().Next(allLines.Length)];
+
+                return name;
+            }
+        }
+      
+    }
+}
